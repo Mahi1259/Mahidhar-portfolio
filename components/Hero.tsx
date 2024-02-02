21aa4785c6faff7ed79cbb26d705cdeef9622a6c
MAHIDHAR REDDY VAKA <mahidhar1337@gmail.com>
2024-02-02 14:31:42 +0000

    updated

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -19,7 +19,7 @@ export default function Home() {
         </Heading>
 
         <p className="text-center md:whitespace-pre-line">
-          {`Dedicated software developer with hands-on experience in full-stack development, cloud computing, and CI/CD practices. Proficient in JavaScript and TypeScript, with a specialization in React for creating engaging user interfaces. Skilled in crafting inventive software solutions, tailoring them to meet both user requirements and technical complexities. Well-versed in cloud computing, particularly AWS, and experienced in containerization with Docker, ensuring seamless deployment and scalability. Committed to continuous professional growth, I am eager to contribute my expertise to dynamic technology teams, driving innovation and achieving exceptional results.`}
+          {`Dedicated software developer with hands-on experience in full-stack development, cloud computing, and CI/CD practices. Proficient in JavaScript and TypeScript, with a specialization in React for creating engaging user interfaces. Skilled in crafting inventive software solutions, tailoring them to meet both user requirements and technical complexities. Well-versed in cloud computing, particularly AWS, and experienced in containerization with Docker, ensuring seamless deployment and scalability. Committed to continuous professional growth, I am eager to contribute my expertise to dynamic technology teams, driving innovation and achieving exceptional results`}
         </p>
       </div>
       <Float config={home} />
